b179c6aee1ec45e404ab2a8aa8fef75689c1d354
Vovizwe <mnmbata@gmail.com>
2022-10-19 14:39:13 -0700

    now

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the abs value of an integer
+ * @t: is the integer to be computed
+ * Return: the abs value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
