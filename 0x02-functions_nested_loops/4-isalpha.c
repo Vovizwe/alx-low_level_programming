0dd4d8f68bcefd47f54996894d88ca9affb9e269
Vovizwe <mnmbata@gmail.com>
2022-10-19 14:05:47 -0700

    new

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 
 /**
  * _isalpha - checks for char alphabet
- * @c: the char to be checked
+ * @c: the character to be checked
  *
- * Return: 1 if char is lower or upper , 0 otherwise
+ * Return: 1 if char is a letter,0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' &&  <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
