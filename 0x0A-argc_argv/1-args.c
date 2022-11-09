73549cbce40cc3c87051e0b3aad092ae7f93a032
Vovizwe <mnmbata@gmail.com>
2022-11-09 01:56:27 -0800

    ttask 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - prints all arguments it receives.
+ * main - prints the number of arguments passed into it.
  * @argc: argument count
  * @argv: arguments
  * Return: 0
  */
 int main(int argc, char **argv)
 {
-	int i;
+	(void)argv;
 
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
+
